66ddb9c663702a6004ef2add3c9e190eeb590311
叶红伟 <yhw@hxcytech.com>
2019-08-05 09:14:55 +0800

    删除无用文件

--- auth/auth-server/src/main/java/com/yhw/auth/server/config/WebSecurityConfigurer.java
+++ auth/auth-server/src/main/java/com/yhw/auth/server/config/WebSecurityConfigurer.java
@@ -3,15 +3,16 @@ package com.yhw.auth.server.config;
 import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Primary
-@Order(90)
+/**
+ * @author
+ */
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
 
@@ -22,10 +23,14 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .antMatchers("/actuator/**", "/token/**","/aaa")
                 .permitAll()//任何人都可以访问上述URL
                 .anyRequest()
-                .authenticated()//必须要经过认证的才能访问
-                .and()
-                .csrf()//允许跨域
-                .disable();
+                .permitAll();
+//                .and()
+//                .logout()
+//                .permitAll()
+//                .authenticated()//必须要经过Basic认证的才能访问
+//                .and()
+//                .csrf()//允许跨域
+//                .disable();
 
     }
 
