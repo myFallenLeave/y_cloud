26010e441c7fc6e1e9557e57dc8bc15a7f439245
yehongwei <yhw@hxcytech.com>
2019-09-23 11:23:22 +0800

    添加配置中心配置

--- config/src/main/java/com/yhw/ConfigApplication.java
+++ config/src/main/java/com/yhw/ConfigApplication.java
@@ -16,6 +16,8 @@ public class ConfigApplication {
 	 * 关于配置中心客户端加载 http://localhost:8888 的问题
 	 * org.springframework.cloud.config.client.ConfigClientProperties 查看客户端配置信息
 	 *
+	 * 需要配置spring cloud bus 实现配置文件动态刷新
+	 *
 	 */
 
 	public static void main(String[] args) {
